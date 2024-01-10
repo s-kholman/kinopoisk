34bdb5573f4345c8621971d4ffa170a986946a1b
Sergey <uporovo@mail.ru>
2024-01-10 19:50:02 +0500

    ...

--- config/routes.php
+++ config/routes.php
@@ -7,5 +7,5 @@ use App\Kernel\Router\Route;
 return [
     Route::get('/home', [HomeController::class, 'index']),
     Route::get('/movies', [MovieController::class, 'index']),
-
+    Route::get('/', [HomeController::class, 'index']),
 ];
